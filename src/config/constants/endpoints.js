94761e7d6c1b2350c5bb9fc93508d98b07b46975
qolkrex <qolkrex@gmail.com>
2023-05-26 16:03:05 -0500

    vacunas img visibles

--- src/config/constants/endpoints.js
+++ src/config/constants/endpoints.js
@@ -11,8 +11,11 @@ export const URL_WAR_TESNET = "https://test.firulaixcoin.finance/";
 export const URL_EXPLORER_MAINET = "https://moonriver.moonscan.io/";
 export const URL_EXPLORER_TESNET = "https://moonbase.moonscan.io/";
 
-export const IPFS_MAINET = "https://gateway.pinata.cloud/ipfs/";
-export const IPFS_TESNET = "https://gateway.pinata.cloud/ipfs/";
+//export const IPFS_MAINET = "https://gateway.pinata.cloud/ipfs/";
+//export const IPFS_TESNET = "https://gateway.pinata.cloud/ipfs/";
+
+export const IPFS_MAINET = "https://ipfs.io/ipfs/";
+export const IPFS_TESNET = "https://ipfs.io/ipfs/";
 
 export const API_MAINET = {
   firulaix: "https://firuapi1.firulaixcoin.finance/api/",
