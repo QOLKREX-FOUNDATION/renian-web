d7c994be37e8a34a54c5dbb454ef21ed34b9773d
romelx23 <romelx23@gmail.com>
2023-07-21 17:13:10 -0500

    updated species renian

--- src/config/constants/races.js
+++ src/config/constants/races.js
@@ -1,8 +1,15 @@
 import DOG from "../../../public/json/data/selects/race/Dogs.json";
 import CAT from "../../../public/json/data/selects/race/Cats.json";
+import RABBIT from "../../../public/Json/data/selects/race/Rabbits.json";
+import MACAW from "../../../public/Json/data/selects/race/Macaw.json";
+import FERRET from "../../../public/Json/data/selects/race/Ferret.json";
+import GUINEAPIG from "../../../public/Json/data/selects/race/GuineaPig.json";
 
 export const racesJson = {
   DOG,
   CAT,
-
+  RABBIT,
+  MACAW,
+  FERRET,
+  GUINEAPIG,
 };
