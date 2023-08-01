fb50cb52d5e991bbccd44d90f310089f99199eb6
romelx23 <romelx23@gmail.com>
2023-08-01 15:55:22 -0500

    fixed race bulldog frances

--- src/hook/inputs/useSpecies.js
+++ src/hook/inputs/useSpecies.js
@@ -1,47 +1,60 @@
 import { useEffect, useMemo, useState } from "react";
 import specieJson from "../../../public/json/data/selects/species.json";
 import { racesJson } from "../../config/constants/races";
+import { firuApi } from "../../../api/firuApi";
 // import { LangContext } from "../../contexts/Localization/LangContext";
 
 const lang = { locale: "es-Es" };
 
 export const useSpecie = (specie) => {
-	// const { lang } = useContext(LangContext);
-	const [species, setSpecies] = useState([]);
-	const [races, setRaces] = useState([]);
+  // const { lang } = useContext(LangContext);
+  const [species, setSpecies] = useState([]);
+  const [races, setRaces] = useState([]);
 
-	const handleSpecies = useMemo(() => {
-		const temp = [];
-		for (let i = 0; i < specieJson.length; i++) {
-			temp.push({
-				label: specieJson[i][lang.locale],
-				value: specieJson[i].value,
-			});
-		}
-		return temp;
-	}, [lang.locale]);
+  const handleSpecies = useMemo(() => {
+    const temp = [];
+    for (let i = 0; i < specieJson.length; i++) {
+      temp.push({
+        label: specieJson[i][lang.locale],
+        value: specieJson[i].value,
+      });
+    }
+    return temp;
+  }, [lang.locale]);
 
-	const handleRaces = useMemo(() => {
-		const temp = [];
-		for (let i = 0; i < racesJson[specie]?.length; i++) {
-			temp.push({
-				label: racesJson[specie][i][lang.locale],
-				value: racesJson[specie][i].value,
-			});
-		}
-		return temp;
-	}, [lang.locale, specie]);
+  const handleRaces = useMemo(() => {
+    const temp = [];
+    for (let i = 0; i < racesJson[specie]?.length; i++) {
+      temp.push({
+        label: racesJson[specie][i][lang.locale],
+        value: racesJson[specie][i].value,
+      });
+    }
 
-	useEffect(() => {
-		setSpecies(handleSpecies);
-	}, [lang.locale, handleSpecies]);
+    // if (specie === "") return temp;
+    // firuApi.get(`races/type?type=${specie}`).then((res) => {
+    //   console.log(res);
+    //   for (let i = 0; i < res.data.races.length; i++) {
+    //     temp.push({
+    //       label: res.data.races[i]["nameSpanish"],
+    //       value: res.data.races[i].value,
+    //     });
+    //   }
+    // });
 
-	useEffect(() => {
-		setRaces(handleRaces);
-	}, [lang.locale, handleRaces]);
+    return temp;
+  }, [lang.locale, specie]);
 
-	return {
-		species,
-		races,
-	};
+  useEffect(() => {
+    setSpecies(handleSpecies);
+  }, [lang.locale, handleSpecies]);
+
+  useEffect(() => {
+    setRaces(handleRaces);
+  }, [lang.locale, handleRaces]);
+
+  return {
+    species,
+    races,
+  };
 };
