9630597c7fc70320ccaccdf535b2361d0f966c3a
romelx23 <romelx23@gmail.com>
2023-09-28 18:01:10 -0500

    update message status search

--- src/components/molecules/consult/DataContent/DataContent.jsx
+++ src/components/molecules/consult/DataContent/DataContent.jsx
@@ -255,7 +255,8 @@ export const ContentMongoPet = ({ dataPet }) => {
 								</div>
 								<div className="flex flex-col">
 									<span className="text-sm">
-										Si deseas cambiar el estado de tu mascota de activo a perdido o robado <b>¡Comunícate con RENIAN!</b> De inmediato modificaremos su estado y colocaremos tu número de contacto.
+										{/* Si deseas cambiar el estado de tu mascota de activo a perdido o robado <b>¡Comunícate con RENIAN!</b> De inmediato modificaremos su estado y colocaremos tu número de contacto. */}
+										Si deseas cambiar el estado de tu mascota de activo a perdido o robado  <b>¡Comunícate con RENIAN!</b> De inmediato modificaremos su estado y colocaremos tu número de contacto.
 									</span>
 								</div>
 							</div>
@@ -263,18 +264,31 @@ export const ContentMongoPet = ({ dataPet }) => {
 						{
 							(dataPet.pet.status === "LOST" || dataPet.pet.status === "STOLEN") &&
 							<div className="flex items-center gap-3 py-3 px-2 text-orange-600 border-2 border-orange-600 rounded-2xl">
-								<div className="w-44">
+								<div className="w-44 flex justify-center ">
 									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16">
 										<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
 									</svg>
 								</div>
-								<div className="flex flex-col">
-									<span className="text-sm">
-										Si deseas cambiar el estado de tu mascota de activo a perdido o robado <b>¡Comunícate con RENIAN!</b> De inmediato modificaremos su estado y colocaremos tu número de contacto.
+								<div className="flex flex-col ">
+									<span className="text-base">
+										{/* Si deseas cambiar el estado de tu mascota de activo a perdido o robado <b>¡Comunícate con RENIAN!</b> De inmediato modificaremos su estado y colocaremos tu número de contacto. */}
+										Mis dueños me es tan buscando, puedes llamar a este número:
 									</span>
-									<span>
+									<a
+										className="bg-blue-600 text-white justify-center px-4 py-2 rounded-2xl font-semibold mt-3 gap-2"
+										href={`tel: +${ dataPet.adopter.phone }`}
+									>
+										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+											<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+										</svg>
+
+										<span>
+											{dataPet?.adopter?.phone}
+										</span>
+									</a>
+									{/* <span>
 										contactarce a : 996844856
-									</span>
+									</span> */}
 								</div>
 							</div>
 						}
